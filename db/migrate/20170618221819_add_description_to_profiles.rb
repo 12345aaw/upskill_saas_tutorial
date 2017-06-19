ba160816677814977101d42d17098b9696014a3b
Andrew Willis <willis.andrew.a@gmail.com>
2017-06-19 02:08:41 +0000

    Homepage UI improvements

--- db/migrate/20170618221819_add_description_to_profiles.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToProfiles < ActiveRecord::Migration[5.0]
-  def change
-    add_column :profiles, :description, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20170619002902) do
     t.string   "job_title"
     t.string   "phone_number"
     t.string   "contact_email"
-    t.         "text"
     t.text     "description"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
